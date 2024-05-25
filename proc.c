f93027fc5c98f5bfb9648d236d7ce7b60881563e
Kim Jihwan <kjhwan0802@naver.com>
2024-05-25 19:47:20 +0900

    mod

--- proc.c
+++ proc.c
@@ -366,7 +366,6 @@ scheduler(void)
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
-      break;
     }
     release(&ptable.lock);
   }
--- rand.h
+++ rand.h
@@ -1,11 +1,9 @@
 #ifndef RAND_H
 #define SIZE 5
 
-//int rand_array[SIZE] = {10, 20, 30, 40, 50};
-static int rand_array[SIZE] = {52, 40, 30, 20, 10};
-static int rand_index;
-
-
+//static int rand_array[SIZE] = {10, 20, 30, 40, 50};
+static int rand_array[SIZE] = {50, 40, 30, 20, 10};
+static int rand_index = 0;
 
 int random(void);
 #endif
